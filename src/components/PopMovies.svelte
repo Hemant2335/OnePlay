5ede7f8679fc04444c2c08f8e840b93d9e0a0af4
Hemant2335 <knrt73373@gmail.com>
2023-10-03 10:36:30 +0530

    Hooks_established

--- src/components/PopMovies.svelte
+++ src/components/PopMovies.svelte
@@ -1,12 +1,26 @@
 <script>
+    import { onMount } from "svelte"; 
+    import {useFetch} from "../hooks/useFetch.js";
     export let movies;
+    $: popular = [];
+    async function load() {
+      try {
+        const endpoint = "popular";
+        popular = await useFetch(endpoint);  
+        console.log(popular);    
+      } catch (error) {
+        console.log(error)
+      }
+    }
+      
+    onMount(load);
 </script>
 
-
-<div class="p-4">
-    <h1 class="text-xl font-semibold">Popular</h1>
+<h1 class="text-2xl font-semibold p-4">Popular</h1>
+<div class="p-[5vh]  flex flex-col">
+    
     <div class="grid grid-cols-4">
-        {#each movies as movie}
+        {#each popular as movie}
         <div class="p-2">
             <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt="poster" class="w-[15vw] rounded-xl object-cover">
             <p class="text-sm font-semibold">{movie.original_title}</p>
---
+++ src/components/Poster.svelte
@@ -0,0 +1,5 @@
+<div class="h-[50vh] w-full p-[5vh]">
+	<div class="h-full w-full bg-red-400">
+		<h1>hloo</h1>
+	</div>
+</div>
